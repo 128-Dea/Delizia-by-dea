090fa2b9fad754a406050683bfc3239c5fef9213
128-Dea <24111814128@mhs.unesa.ac.id>
2025-10-11 13:00:07 +0700

    Rebuild Flutter web for GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -63,7 +63,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5c1886a7c65d4b24d8c14d3c12439295
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "92f317fdb46b2d263210775d2f5c54a0",
+"flutter_bootstrap.js": "e7fd08d7910400f0186eb884a8ffe02a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
